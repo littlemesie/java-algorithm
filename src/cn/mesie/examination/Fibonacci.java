7e237bea0f0d2e37892b82e962459b4033a39b16
littlemesie <1319417676@qq.com>
2017-03-09 20:35:21 +0800

    happy day

---
+++ src/cn/mesie/examination/Fibonacci.java
@@ -0,0 +1,25 @@
+package cn.mesie.examination;
+/**
+ *  
+ * @author mesie
+ *
+ * Mar 9, 2017 8:27:02 PM
+ */
+public class Fibonacci {
+	public long fibonacci(int m){
+		long sum = 0;
+		if(m == 1){
+			sum = 1;
+		}else if(m == 2) {
+			sum = 2;
+		}else {
+			sum = fibonacci(m-1) + fibonacci(m -2);;
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		Fibonacci f = new Fibonacci();
+		long s = f.fibonacci(10);
+		System.out.println(s);
+	}
+}
