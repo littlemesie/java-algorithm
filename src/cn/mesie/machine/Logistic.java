007bb556c8ad8558e59d5f5fd447469b5ca6eea0
littlemesie <1319417676@qq.com>
2017-03-19 12:42:22 +0800

    Logistich回归算法

---
+++ src/cn/mesie/machine/Logistic.java
@@ -0,0 +1,12 @@
+package cn.mesie.machine;
+/**
+ * logistic »Ø¹é
+ * @author mesie
+ *
+ * Mar 19, 2017 12:25:20 PM
+ */
+public class Logistic {
+	public static void main(String[] args) {
+		System.out.println(123);
+	}
+}
